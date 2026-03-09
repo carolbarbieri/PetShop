75f3ac3ece3bf5a7454f1033d12abe49249db468
carolbarbieri <carolinabarbieri290508@gmail.com>
2026-03-09 08:33:11 -0300

    Ajustando os controllers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import mainRouter from './routes/index';
 
 dotenv.config();
 
